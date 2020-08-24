e7ac7a8e3478f91dd5f88d774643207b79e565dc
Abdul Rehman <a.rehmans@gmail.com>
2020-08-24 12:28:02 +0400

    Update start_tomo.sh

--- Tomochain/scripts/start_tomo.sh
+++ Tomochain/scripts/start_tomo.sh
@@ -12,4 +12,24 @@ sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' 
 echo "export VISIBLE=now" >> /etc/profile
 echo "127.0.0.1 localhost" > /etc/hosts
 
+mkdir -pv /opt/data && mkdir -pv /opt/keys
+DATA_DIR=/opt/data
+KEYSTORE_DIR=/opt/keys
+
+touch /opt/keys/pass.txt
+if [ -z "${KEYPASS}" ]; then KEYPASS="tf2020"; fi
+echo ${KEYPASS} > /opt/keys/pass.txt
+
+if [ -z "${IDENTITY}" ]; then IDENTITY="tf-tomo"; fi
+if [ -z "${BOOTNODES}" ]; then BOOTNODES=enode://97f0ca95a653e3c44d5df2674e19e9324ea4bf4d47a46b1d8560f3ed4ea328f725acec3fcfcb37eb11706cf07da669e9688b091f1543f89b2425700a68bc8876@3.212.20.0:30301; fi
+
+# Set TomoChain Stats Server | Default
+NETSTATS_HOST='wss://stats.tomochain.com'
+NETSTATS_PORT='443'
+WS_SECRET='getty-site-pablo-auger-room-sos-blair-shin-whiz-delhi'
+
+# Setup New TomoChain Account and start Full Node
+/opt/tomo account new --password $KEYSTORE_DIR/pass.txt --keystore $KEYSTORE_DIR
+/opt/tomo --syncmode "full" --announce-txs --datadir $DATA_DIR --networkid $NETWORK_ID --port 30303 --keystore $KEYSTORE_DIR --password $KEYSTORE_DIR/pass.txt --identity $IDENTITY --mine --gasprice 250000000 --bootnodes $BOOTNODES --ethstats $IDENTITY:$WS_SECRET@$NETSTATS_HOST:$NETSTATS_PORT
+
 exec /usr/sbin/sshd -D
