590a3fb454794994366d3d2fd8f2be3c487150ff
abdulgig <a.rehmans@gmail.com>
2020-08-27 19:47:59 +0000

    code update

--- Harmony/scripts/start_hmy.sh
+++ Harmony/scripts/start_hmy.sh
@@ -28,8 +28,8 @@ mv *.key $KEYS_DIR
 
 if [ -z "${network}" ]; then network="mainnet"; fi
 cd /opt && mkdir -p /opt/extras && mv banner /opt/extras && mv setmotd /opt/extras
-/opt/extras/setmotd $shard $network
 
 nohup /opt/harmony --network $network --bls.dir $KEYS_DIR --bls.pass.file $KEYS_DIR/keypass.txt > /dev/null 2>&1 &
+/opt/extras/setmotd $shard $network $blskeycount
 
 exec /usr/sbin/sshd -D
