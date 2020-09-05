de39920519de2771fd685cb11e8f8962c0c9816e
abdulgig <a.rehmans@gmail.com>
2020-09-05 19:23:56 +0000

    code update

---
+++ Digibyte/scripts/start_dgb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir ~/.ssh
+mkdir -p /var/run/sshd
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chmod 600 /etc/ssh/ssh_host_*
+echo $pub_key >> /root/.ssh/authorized_keys
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+echo "export VISIBLE=now" >> /etc/profile
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "deb http://be.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
+
+exec /usr/sbin/sshd -D
