06f3426f0da6b2be731fd0d8d6f9c492e17f1df4
abdulgig <a.rehmans@gmail.com>
2020-11-16 22:00:33 +0000

    helm ssh

--- Digibyte/scripts/start_dgb.sh
+++ Digibyte/scripts/start_dgb.sh
@@ -7,7 +7,7 @@ touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chmod 600 /etc/ssh/ssh_host_*
 echo $pub_key >> /root/.ssh/authorized_keys
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 echo "export VISIBLE=now" >> /etc/profile
 echo "127.0.0.1 localhost" >> /etc/hosts
