a061f9fb291becb38d2d678fea16f186000f4523
abdulgig <a.rehmans@gmail.com>
2020-11-27 19:41:59 +0000

    tomo helm update

--- Tomochain/scripts/start_tomo.sh
+++ Tomochain/scripts/start_tomo.sh
@@ -7,19 +7,21 @@ touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chmod 600 /etc/ssh/ssh_host_*
 echo $pub_key >> /root/.ssh/authorized_keys
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 echo "export VISIBLE=now" >> /etc/profile
 echo "127.0.0.1 localhost" > /etc/hosts
 echo "deb http://be.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
 
-mkdir -pv /opt/data && mkdir -pv /opt/keys
-DATA_DIR=/opt/data
-KEYSTORE_DIR=/opt/keys
+BASE_DIR=/tomodata
 
-touch /opt/keys/pass.txt
+mkdir -pv $BASE_DIR/data && mkdir -pv $BASE_DIR/keys
+DATA_DIR=$BASE_DIR/data
+KEYSTORE_DIR=$BASE_DIR/keys
+
+touch $KEYSTORE_DIR/pass.txt
 if [ -z "${KEYPASS}" ]; then KEYPASS="tf2020"; fi
-echo ${KEYPASS} > /opt/keys/pass.txt
+echo ${KEYPASS} > $KEYSTORE_DIR/pass.txt
 
 if [ -z "${IDENTITY}" ]; then IDENTITY="tf-tomo"; fi
 if [ -z "${BOOTNODES}" ]; then BOOTNODES=enode://97f0ca95a653e3c44d5df2674e19e9324ea4bf4d47a46b1d8560f3ed4ea328f725acec3fcfcb37eb11706cf07da669e9688b091f1543f89b2425700a68bc8876@3.212.20.0:30301; fi
