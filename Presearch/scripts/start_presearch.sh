d8352d056104f4f953f8ea45dffe5da684a57404
Abdul Rehman <a.rehmans@gmail.com>
2021-03-03 15:01:09 +0400

    Update start_presearch.sh

--- Presearch/scripts/start_presearch.sh
+++ Presearch/scripts/start_presearch.sh
@@ -2,11 +2,8 @@
 REGISTRATION_CODE=`echo $REGISTRATION_CODE | tr -d '"'`
 echo $REGISTRATION_CODE > /tmp/checkcode
 echo OK > /tmp/checkps
-echo "apache ALL=(ALL) NOPASSWD: /usr/bin/checkupg" >> /etc/sudoers
 rm -f /var/www/localhost/htdocs/index.html
 /usr/sbin/httpd -k start
-cat /tmp/cronjobs | crontab -
-rm -rf /var/www/localhost/htdocs/.git
 ln -s /tmp/checklogs /var/www/localhost/htdocs/node.log  
 /usr/sbin/crond -b
-exec /app/presearch-node >> /tmp/checklogs 
+exec /app/presearch-node >> /tmp/checklogs
